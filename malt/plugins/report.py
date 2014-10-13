21101bf942579c9c3f273f6156e178427ed33b45
Darren Mulholland <dmulholland@outlook.ie>
2014-10-13 11:39:06 +0100

    Add status report plugin

---
+++ malt/plugins/report.py
@@ -0,0 +1,16 @@
+"""
+This sample plugin prints a simple status report at the end of each build.
+
+"""
+
+from .. import hooks
+from .. import site
+
+
+@hooks.register('exit')
+def print_status_report():
+    pcount = site.page_count()
+    btime = site.build_time()
+    average = btime / (pcount or 1)
+    status = "%s pages rendered in %.2f seconds. %.4f seconds per page."
+    print(status % (pcount, btime, average))
