d0db95d47169423541de848c7c14fa9edbfa92d9
Darren Mulholland <dmulholland@outlook.ie>
2015-03-21 18:17:01 +0000

    Housekeeping

--- ark/tags.py
+++ ark/tags.py
@@ -5,16 +5,16 @@ from . import site
 from .utils import slugify
 
 
-# Maps tag slugs to lists of record filepaths indexed by type.
+# Maps tag-slugs to lists of record-filepaths indexed by type.
 _rmap = {}
 
-# Maps tag slugs to tag names indexed by type.
+# Maps tag-slugs to tag-names indexed by type.
 _nmap = {}
 
 
 class TagInfo:
 
-    """ Pairs a tag name with its corresponding tag-index url. """
+    """ Pairs a tag-name with its corresponding tag-index url. """
 
     def __init__(self, name, url):
         self.name = name
@@ -34,17 +34,17 @@ def register(typeid, tag, filepath):
 
 
 def records():
-    """ Returns the dictionary of registered tag to record mappings. """
+    """ Returns the dictionary of registered tag-to-record mappings. """
     return _rmap
 
 
 def names():
-    """ Returns the dictionary of tag slug to tag name mappings. """
+    """ Returns the dictionary of tag-slug-to-tag-name mappings. """
     return _nmap
 
 
 def slugs(typeid, tag, *append):
-    """ Returns the output slug list for the specified tag. """
+    """ Returns the output-slug list for the specified tag. """
     slugs = site.slugs(typeid)
     slugs.extend(s for s in site.type(typeid)['tag_slug'].split('/') if s)
     slugs.append(slugify(tag))
@@ -53,5 +53,5 @@ def slugs(typeid, tag, *append):
 
 
 def url(typeid, tag):
-    """ Returns the index page url for the specified tag. """
+    """ Returns the index-page url for the specified tag. """
     return site.url(slugs(typeid, tag, 'index'))
