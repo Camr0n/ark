cd173e4d3a497558f8aa7bd485f8411cd8363229
Darren Mulholland <dmulholland@outlook.ie>
2015-08-22 10:42:36 +0100

    Housekeeping

--- ark.py
+++ ark.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
+"""
+Ark: a static website generator in Python 3.
+
+Ark transforms a directory of text files written in Syntex or Markdown
+into a static website that can be viewed locally or served remotely.
+
+This script functions as the application's entry point. It is only
+used during development as installing Ark via pip automatically generates
+a new entry point script on the user's PATH.
+
+Author: Darren Mulholland <dmulholland@outlook.ie>
+License: Public Domain
+
+"""
 
 import ark
 
+# The cli module handles Ark's command line interface.
 ark.cli.cli()
