a9f2e136da7a9c939583bf3fc582467aaebc2b84
Darren Mulholland <dmulholland@outlook.ie>
2015-08-26 20:45:26 +0100

    Better error reporting for template extensions

--- ark/ext/ibis_support.py
+++ ark/ext/ibis_support.py
@@ -21,27 +21,17 @@ def init():
 @ark.templates.register('ibis')
 def callback(page, filename):
 
-    # Load the template object.
     try:
         template = ibis.config.loader(filename)
-    except ibis.errors.TemplateError as e:
-        msg =  'Ibis template error loading file: %s\n\n' % filename
-        msg += '  %s: %s' % (e.__class__.__name__, e)
-        if e.__context__:
-            msg += '\n\n  %s: %s' % (
-                e.__context__.__class__.__name__, e.__context__
-            )
-        sys.exit(msg)
-
-    # Render the page.
-    try:
         return template.render(page)
     except ibis.errors.TemplateError as e:
-        msg =  'Ibis template error rendering page:\n'
-        msg += '  %s\n\n' % page['path']
-        msg += '  %s: %s' % (e.__class__.__name__, e)
+        msg =  "-----------------------\n"
+        msg += "  Ibis Template Error  \n"
+        msg += "-----------------------\n\n"
+        msg += "  Template: %s\n" % filename
+        msg += "  Page:     %s\n\n" % page['path']
+        msg += "  %s: %s" % (e.__class__.__name__, e)
         if e.__context__:
-            msg += '\n\n  %s: %s' % (
-                e.__context__.__class__.__name__, e.__context__
-            )
+            msg += "\n\nThe error was caused by the following exception:\n\n"
+            msg += "%s: %s" % (e.__context__.__class__.__name__, e.__context__)
         sys.exit(msg)
--- ark/ext/jinja_support.py
+++ ark/ext/jinja_support.py
@@ -36,27 +36,17 @@ def init():
 @ark.templates.register('jinja')
 def callback(page, filename):
 
-    # Load the template object.
     try:
         template = env.get_template(filename)
-    except jinja2.TemplateError as e:
-        msg =  'Jinja template error loading file: %s\n\n' % filename
-        msg += '  %s: %s' % (e.__class__.__name__, e)
-        if e.__context__:
-            msg += '\n\n  %s: %s' % (
-                e.__context__.__class__.__name__, e.__context__
-            )
-        sys.exit(msg)
-
-    # Render the page.
-    try:
         return template.render(page)
     except jinja2.TemplateError as e:
-        msg =  'Jinja template error rendering page:\n'
-        msg += '  %s\n\n' % page['path']
-        msg += '  %s: %s' % (e.__class__.__name__, e)
+        msg =  "------------------------\n"
+        msg += "  Jinja Template Error  \n"
+        msg += "------------------------\n\n"
+        msg += "  Template: %s\n" % filename
+        msg += "  Page:     %s\n\n" % page['path']
+        msg += "  %s: %s" % (e.__class__.__name__, e)
         if e.__context__:
-            msg += '\n\n  %s: %s' % (
-                e.__context__.__class__.__name__, e.__context__
-            )
+            msg += "\n\nThe error was caused by the following exception:\n\n"
+            msg += "%s: %s" % (e.__context__.__class__.__name__, e.__context__)
         sys.exit(msg)
