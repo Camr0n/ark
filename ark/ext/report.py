1dbb9eb3162bc2e75f2dcc332a9c81bcd3d0b1b3
Darren Mulholland <dmulholland@outlook.ie>
2015-09-02 04:37:38 +0100

    Fix page/pages plural bug in the 'report' plugin

--- ark/ext/report.py
+++ ark/ext/report.py
@@ -12,9 +12,10 @@ from ark import hooks, site
 # Register a callback on the 'exit' event hook to print our status report.
 @hooks.register('exit')
 def print_status_report():
-    rendered, written = site.page_count()
+    num_rendered, num_written = site.page_count()
+    rendered = "1 page" if num_rendered == 1 else "%s pages" % num_rendered
+    written = "1 page" if num_written == 1 else "%s pages" % num_written
     time = site.build_time()
-    average = time / (rendered or 1)
-    status = "%s pages rendered, %s pages written in %.2f seconds. "
-    status += "%.4f seconds per page."
+    average = time / (num_rendered or 1)
+    status = "%s rendered, %s written in %.2f seconds. %.4f seconds per page."
     print(status % (rendered, written, time, average))
