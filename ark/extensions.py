7c16ebc6963a95ea81f17c9d4f4ce20864937b27
Darren Mulholland <dmulholland@outlook.ie>
2015-10-03 21:22:06 +0100

    Add support for a global extensions directory

--- ark/extensions.py
+++ ark/extensions.py
@@ -15,18 +15,26 @@ _loaded = {}
 
 # Loads any Python modules found in the extensions directories.
 def load():
-    dirs = [os.path.join(os.path.dirname(__file__), 'ext')]
+    dirs = []
+
+    # Default extensions bundled with Ark.
+    dirs.append(os.path.join(os.path.dirname(__file__), 'ext'))
+
+    # Global extensions directory.
+    if os.getenv('ARK_EXTENSIONS') and os.path.isdir(os.getenv('ARK_EXTENSIONS')):
+        dirs.append(os.getenv('ARK_EXTENSIONS'))
+
+    # Site-specific extensions.
     if os.path.isdir(site.ext()):
         dirs.append(site.ext())
+
+    # Load extensions.
     for dirpath in dirs:
         sys.path.insert(0, dirpath)
-        names = [
-            os.path.splitext(name)[0]
-                for name in os.listdir(dirpath)
-                    if not name[0] in '_.'
-        ]
-        for name in names:
-            _loaded[name] = importlib.import_module(name)
+        for name in os.listdir(dirpath):
+            base = os.path.splitext(name)[0]
+            if not base[0] in '_.':
+                _loaded[base] = importlib.import_module(base)
         sys.path.pop(0)
 
 
