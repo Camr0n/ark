3b57981773704c9efec0fc4984f4e756da225d72
Darren Mulholland <dmulholland@outlook.ie>
2016-02-03 23:30:32 +0000

    Add optional order parameter for hook handlers

--- ark/hooks.py
+++ ark/hooks.py
@@ -2,28 +2,30 @@
 # Event and filter hooks.
 # --------------------------------------------------------------------------
 
-# Maps hook names to lists of callback functions.
+# Maps hook names to lists of callback functions indexed by order.
 _handlers = {}
 
 
 # Decorator function for registering event and filter handlers.
-def register(hook):
+def register(hook, order=0):
 
-    def callback(func):
-        _handlers.setdefault(hook, []).append(func)
+    def register_handler(func):
+        _handlers.setdefault(hook, {}).setdefault(order, []).append(func)
         return func
 
-    return callback
+    return register_handler
 
 
 # Fires an event hook.
 def event(hook, *args):
-    for func in _handlers.get(hook, []):
-        func(*args)
+    for order in sorted(_handlers.get(hook, {})):
+        for func in _handlers[hook][order]:
+            func(*args)
 
 
 # Fires a filter hook.
 def filter(hook, value, *args):
-    for func in _handlers.get(hook, []):
-        value = func(value, *args)
+    for order in sorted(_handlers.get(hook, {})):
+        for func in _handlers[hook][order]:
+            value = func(value, *args)
     return value
